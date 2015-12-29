6ae01e134889e23a529ecdb91b1cd91045b11d91
DavidWei <godhuyang@hotmail.com>
2015-12-30 00:05:55 +0800

    Add docker and mysql configration

--- provision/scripts/apt-update.sh
+++ provision/scripts/apt-update.sh
@@ -1,9 +1,6 @@
 sudo sed -i 's!http://\(.*archive\|security\)\.ubuntu\.com!http://cn.archive.ubuntu.com!' /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install -y grub-pc
-echo -e '#!/bin/bash\necho update-grub placeholder' > /tmp/update-grub.tmp
-chmod a+x /tmp/update-grub.tmp
-sudo bash -c 'mv /usr/sbin/update-grub /usr/sbin/update-grub.orig; mv /tmp/update-grub.tmp /usr/sbin/update-grub'
+sudo su -c "echo 'set grub-pc/install_devices /dev/sda' | debconf-communicate"
 sudo apt-get -y upgrade
 echo "Install common software ..."
 sudo apt-get install -y software-properties-common
